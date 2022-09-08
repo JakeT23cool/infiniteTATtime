118c0578cbc93bcbdb28ed40e98150cb483a800b
JakeT23 <69044307+JakeT23cool@users.noreply.github.com>
2022-09-08 18:28:36 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        near (In)finite Test&Track Time
+// @namespace   Violentmonkey Scripts
+// @include     https://www.testandtrack.io/index.php/profile/givetest
+// @grant       none
+// @version     1.0
+// @author      jaket23
+// @description 08/09/2022, 18:20:03
+// ==/UserScript==
+
+c = 99999999; // crazy programming
